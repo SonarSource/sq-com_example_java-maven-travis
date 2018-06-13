a69ff72c764da3b98fbfe4859332793aa47aa5ff
Satish Kumar Anandhan <iamsatish@live.com>
2018-06-13 13:36:43 +0530

    Modified for the changes.

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -7,7 +7,7 @@ public class Foo {
 
     public static int div(int a, int b) throws Exception {
     	if (b == 0) {
-    		throw new UnsupportedOperationException("Can't divide by zero!");
+    		throw new UnsupportedOperationException("Can't divide by zero yup!");
     	}
         return a / b;  
     }
